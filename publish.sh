32fa3004ebde8e1b71e3b2683efd09d47ddd017c
jura-gresko <juragresko@gmail.com>
2020-03-25 10:01:34 -0300

    update wheel name for OS X

--- publish.sh
+++ publish.sh
@@ -3,5 +3,9 @@
 function upload_to_pypi {
     local pypi_server=${1:-$PYPI_SERVER}
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
-    TWINE_USERNAME=__token__ TWINE_PASSWORD=${PYPI_KEY} twine upload --repository-url ${pypi_server} ${wheelhouse}/*-manylinux*.whl
+
+    local OS_ID=manylinux
+    if [ -n "$IS_OSX" ]; then OS_ID=macosx; fi
+
+    TWINE_USERNAME=__token__ TWINE_PASSWORD=${PYPI_KEY} twine upload --repository-url ${pypi_server} ${wheelhouse}/*-${OS_ID}*.whl
 }
